d34a23b8be40ff3438331b428a71d88ce5a60482
Naftali Weiss <Naftali@PROPUMP.local>
2020-07-23 12:50:03 -0400

    formatted the checkout page a bit and anded functionallity to remove all items from the cart

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {removeFromCart} from '../actions/shoppingCart'
 
 
 class CheckoutPage extends Component{
 
-
+    fullItemRemoval = (id, quantity) => {
+        for (let i = 0; i < quantity; i++) {
+            this.props.removeFromCart(id, true)
+        }
+    }
 
     render(){
         const itemsInCart = !this.props.cart? "Loading" : this.props.cart.map((item, index)=>{
-            if(item.quantity > 0){
+            if(item.quantity > 0 || item.quantity2 > 0){
                 return(
                     <div key={index}>
-                        <img src={`http://localhost:3001/${item.img}`}/>
+                        <div style={style.imgContainer}>
+                            <img style={style.imgStyle} src={`http://localhost:3001/${item.img}`}/>
+                            <button style={style.removeBtn} onClick={()=>this.fullItemRemoval(item.id, item.quantity)}>Remove</button>
+                        </div>
                     </div>
                 )
             }
@@ -26,10 +34,25 @@ class CheckoutPage extends Component{
     }
 }
 
+const style = {
+    imgContainer:{
+        border: '1px solid black',
+        width : '300px'
+    },
+    imgStyle:{
+        height:'300px',
+        display: 'block',
+        margin: 'auto'
+    },
+    removeBtn:{
+        display:'block'
+    }
+}
+
 const mapStateToProps = (state) => ({
     total : state.shoppingCart.total,
     cart : state.shoppingCart.cart,
     cartCount : state.shoppingCart.cartCount
 });
 
-export default connect(mapStateToProps, {})(CheckoutPage)
+export default connect(mapStateToProps, {removeFromCart})(CheckoutPage)
